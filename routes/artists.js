137053a6b219d1242965823d3970c7e33eddc776
Mark Lehane <mklehane@gmail.com>
2016-03-08 11:42:05 +0000

    I have no idea what I'm doing
    Removed unused items from navabar
    Cleaned up code

--- routes/artists.js
+++ routes/artists.js
@@ -6,8 +6,9 @@ router.get('/artistlist', function(req, res) {
 	var rand = Math.floor((Math.random() * 2500) + 1);
     var db = req.db;
     var collection = db.get('artists');
-    collection.find({},{ skip: rand, limit: 20 },function(e,docs){
-        res.json(docs);
+    collection.find({},{ skip: rand, limit: 20 },function(e,docs) {
+
+			res.json(docs);
 
     });
 
@@ -17,12 +18,12 @@ router.get('/:id', function(req, res) {
 
 	var db = req.db;
     var collection = db.get('artists');
-    collection.find({ 'id': parseInt(req.params.id) },{ limit: 1 },function(e,docs){
-        res.json(docs);
+    collection.find({ 'id': parseInt(req.params.id) },{ limit: 1 },function(e,docs) {
+
+			res.json(docs);
+
     });
 
-	// res.render('artistdetails', {title: 'Artist Details'});
-	
 });
 
 module.exports = router;
