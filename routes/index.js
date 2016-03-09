da74396db3057e638821ffb6dd80d213144d675a
Mark Lehane <mklehane@gmail.com>
2016-03-09 15:21:02 +0000

    Began work on implementing random artists and artworks into index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+
+	var rand = Math.floor((Math.random() * 2500) + 1);
+	var db = req.db;
+	var collection = db.get('artists');
+	collection.find({},{ skip: rand, limit: 1 },function(e,docs) {
+
+		res.render('index', { "artist" : docs });
+
+	});
+
+});
+
+router.get('/', function(req, res, next) {
+
+	var rand = Math.floor((Math.random() * 2500) + 1);
+	var db = req.db;
+	var collection = db.get('artworks');
+	collection.find({},{ skip: rand, limit: 1 },function(e,docs) {
+
+		res.render('index', { "artwork" : docs });
+
+	});
+
 });
 
 module.exports = router;
