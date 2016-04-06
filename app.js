2adcf47d22b42f3565f8d04f6a6fa2ae64cef287
Mark Lehane <marklehane@users.noreply.github.com>
2016-04-06 10:02:34 +0100

    Changed db to use MLab hosted service

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ var bodyParser = require('body-parser');
 //DB Info
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('localhost:27017/tate2')
+//var db = monk('localhost:27017/tate2')
+var db = monk('mongodb://user:johnnylogan@ds015760.mlab.com:15760/tate2')
 
 //Routing
 var routes = require('./routes/index');
