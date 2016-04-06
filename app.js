9a45d13f99c936e4a70ba98e2b450f2e31991cf1
Mark Lehane <marklehane@users.noreply.github.com>
2016-04-06 10:22:36 +0100

    Bug fix

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var bodyParser = require('body-parser');
 var mongo = require('mongodb');
 var monk = require('monk');
 //var db = monk('localhost:27017/tate2')
-var db = monk('mongodb://user:johnnylogan@ds015760.mlab.com:15760/tate2')
+var db = monk('mongodb://user:johnnylogan@ds015760.mlab.com:15760/tate2');
 
 //Routing
 var routes = require('./routes/index');
